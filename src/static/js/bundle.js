c442b763e89ae5ed8600de2e614821fe70ee1396
ReesKlintworth <rees.klintworth@gmail.com>
2016-10-23 14:03:59 -0500

    Setup server using express.

--- src/static/js/bundle.js
+++ src/static/js/bundle.js
@@ -27970,7 +27970,7 @@
   \*************************************/
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 	
 	Object.defineProperty(exports, "__esModule", {
 	  value: true
@@ -27981,8 +27981,6 @@
 	var _react = __webpack_require__(/*! react */ 1);
 	
 	var _react2 = _interopRequireDefault(_react);
-	
-	var _reactRouter = __webpack_require__(/*! react-router */ 173);
 	
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 	
@@ -28002,19 +28000,15 @@
 	  }
 	
 	  _createClass(IndexPage, [{
-	    key: 'render',
+	    key: "render",
 	    value: function render() {
 	      return _react2.default.createElement(
-	        'div',
-	        { className: 'home' },
+	        "div",
+	        { className: "home" },
 	        _react2.default.createElement(
-	          _reactRouter.Link,
-	          { to: 'notfound' },
-	          _react2.default.createElement(
-	            'p',
-	            null,
-	            'Home page'
-	          )
+	          "p",
+	          null,
+	          "Home page"
 	        )
 	      );
 	    }
